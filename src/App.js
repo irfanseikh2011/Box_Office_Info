ef94b61732e1ac7d947cda20ba359a9555bf1860
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-02-05 21:51:48 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-    </div>
+      <Switch>
+      <Route exact path="/home">
+        <h1>Welcome to the Homepage</h1>
+      </Route> 
+      <Route path="/profile">
+        <h1>You have reached your Profile Page</h1>
+      </Route>
+      <Route>
+        <h1>404 Error! Page not Found.</h1>
+      </Route>
+    </Switch>
   );
 }
 
