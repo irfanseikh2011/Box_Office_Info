d4486584bbcb6aec5ced50f00eb0b9387874f1d6
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-02-11 12:36:46 +0530

    Added a search bar to retrieve data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayouts from '../components/MainPageLayouts'
 
+
+
+
 const Home = () => {
+
+    const [input,setInput] = useState('');
+
+    const onInputChange = (e) => {
+        console.log(e.target.value);
+    }
+
+    const onSearch = () => {
+        //
+        fetch('https://api.tvmaze.com/search/shows?q=girls').then(r => r.json()).then(result => 
+        console.log(result));
+    }
+
+    const onKeyDown = (e) => {
+        if(e.key === 'Enter')
+        {
+            onSearch();
+        }
+    } 
+
     return (
         <MainPageLayouts>
-            <h1>This is the home page.</h1>
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} ></input>
+            <button type="submit" onClick={onSearch}>Search</button>
         </MainPageLayouts>
     )
 }
